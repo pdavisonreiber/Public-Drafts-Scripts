ffde77f51fef85f66b3e3df5ba6c05684b843ed6
Peter Davison-Reiber <pdavisonreiber@gmail.com>
2018-02-18 11:27:31 +0000

    Added project functionality and bug fixes

--- Send to Things/Send to Things.js
+++ Send to Things/Send to Things.js
@@ -1,5 +1,5 @@
-// Dictionary of Projects or Areas Names with ids e.g. {"Name of Project": "id"}
-var presetProjects = {}
+// Array of Projects or Area names. Names should be unique.
+var presetProjects = ["School", "Teaching", "IT", "CPD", "Maths Society"]
 
 // Prompt to select where task will go.
 var targetPrompt = Prompt.create()
@@ -18,70 +18,76 @@ if (!didSelectTarget) {
 	var location = targetPrompt.buttonPressed
 	if (location == "Inbox") {
 		var task = TJSTodo.create()
-		task.title = draft.title
-		task.notes = draft.body
+		task.title = draft.processTemplate("[[title]]")
+		task.notes = draft.processTemplate("[[body]]")
 	} else if (location == "Today") {
 		var task = TJSTodo.create()
-		task.title = draft.title
-		task.notes = draft.body
+		task.title = draft.processTemplate("[[title]]")
+		task.notes = draft.processTemplate("[[body]]")
 		task.when = "today"
 	} else if (location == "Project") {
-		
 		var taskPrompt = Prompt.create()
-		taskPrompt.title = "Task Details"
-		
-		var presetProjectNames = Object.keys(presetProjects)
-		taskPrompt.addSelect("projectChoice", "Preset Projects", presetProjectNames.concat(["Other Project"]), "Other Project", false) 
-		taskPrompt.addTextField("otherProject", "Other Project", "", {placeholder: "Project Name", autocorrect: true, autocapitalization: "words"})
+		taskPrompt.title = "Select Project"
+		taskPrompt.addSelect("projectChoice", "Preset Projects", presetProjects.concat(["Other"]), ["Other"], false) 
+		taskPrompt.addTextField("otherProject", "", "", {placeholder: "Other Project", autocorrect: true, autocapitalization: "words"})
 		taskPrompt.addTextField("heading", "Heading", "", {placeholder: "optional", autocorrect: true, autocapitalization: "words"})
 		
-		var today = new Date()
-		taskPrompt.addSwitch("hasDate", "Date", false)
-		taskPrompt.addDatePicker("date", "Date", today, {mode: "date"})
-		taskPrompt.addSwitch("hasDeadline", "Deadline", false)
-		taskPrompt.addDatePicker("deadline", "Deadline", today, {mode: "date"})
-		
-		taskPrompt.addTextField("tags", "Tags", "", {placeholder: "optional", autocorrect: true})
+		taskPrompt.addTextField("tags", "Tags", "", {placeholder: "tag1, tag2, ...", autocorrect: true})
 		
 		taskPrompt.addButton("OK")
-		
 		var didSelectProject = taskPrompt.show()
 		
 		if (!didSelectProject) {
 			context.cancel()
 		} else {
-		
-			var projectChoice = taskPrompt.fieldValues.projectChoice
-			var otherProject = taskPrompt.fieldValues.otherProject
-			var heading = taskPrompt.fieldValues.heading
-			var hasDate = taskPrompt.fieldValues.hasDate
-			var date = taskPrompt.fieldValues.date
-			var hasDeadline = taskPrompt.fieldValues.hasDeadline
-			var deadline = taskPrompt.fieldValues.deadline
-			var tags = taskPrompt.fieldValues.tags.split(", ")
+			var datePrompt = Prompt.create()
+			datePrompt.title = "Dates"
 			
-			var task = TJSTodo.create()
-			task.title = draft.title
-			task.notes = draft.body
-			if (hasDate) {
-				task.when = date
-			}
-			if (hasDeadline) {
-				task.deadline = deadline
-			}
-			if (projectChoice == "Other Project") {
-				task.list = otherProject
+			var today = new Date()
+			datePrompt.addSwitch("hasDate", "Add Date", false)
+			datePrompt.addDatePicker("date", "Date", today, {mode: "date"})
+			
+			datePrompt.addSwitch("hasDeadline", "Add Deadline", false)
+			datePrompt.addDatePicker("deadline", "Deadline", today, {mode: "date"})
+			
+			datePrompt.addButton("OK")
+			
+			var didSelectDate = datePrompt.show()
+			
+			if (!didSelectDate) {
+				context.cancel()
 			} else {
-				task.list = projectChoice
+				var projectChoice = taskPrompt.fieldValues.projectChoice
+				var otherProject = taskPrompt.fieldValues.otherProject
+				var heading = taskPrompt.fieldValues.heading
+				var tags = taskPrompt.fieldValues.tags.split(", ")
+				
+				var hasDate = datePrompt.fieldValues.hasDate
+				var date = datePrompt.fieldValues.date
+				var hasDeadline = datePrompt.fieldValues.hasDeadline
+				var deadline = datePrompt.fieldValues.deadline
+				var task = TJSTodo.create()
+				task.title = draft.processTemplate("[[title]]")
+				task.notes = draft.processTemplate("[[body]]")
+				if (hasDate) {
+					task.when = date
+				}
+				if (hasDeadline) {
+					task.deadline = deadline
+				}
+				if (projectChoice == "Other") {
+					task.list = otherProject
+				} else {
+					task.list = projectChoice
+				}
+				task.tags = tags
+				task.heading = heading
 			}
-			task.tags = tags
-			task.heading = heading
-			
 		}
 	}
 }
 
-var container = TJSContainer.create(task)
+var container = TJSContainer.create([task])
 var cb = CallbackURL.create()
 cb.baseURL = container.url
 var success = cb.open()
