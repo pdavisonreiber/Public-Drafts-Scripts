b69425c5547f16e2c41452a10df3bd6e16723c60
Peter Davison-Reiber <pdavisonreiber@gmail.com>
2018-02-18 18:42:35 +0000

    Added convert to project option

--- Send to Things/Send to Things.js
+++ Send to Things/Send to Things.js
@@ -1,5 +1,6 @@
 // Array of Projects or Area names. Names should be unique.
 var presetProjects = ["School", "Teaching", "IT", "CPD", "Maths Society"]
+var presetAreas = ["School", "Personal", "Hobbies"]
 
 // Prompt to select where task will go.
 var targetPrompt = Prompt.create()
@@ -7,7 +8,7 @@ var targetPrompt = Prompt.create()
 targetPrompt.title = "Task Location"
 targetPrompt.addButton("Inbox")
 targetPrompt.addButton("Today")
-targetPrompt.addButton("Project")
+targetPrompt.addButton("Existing Project")
 targetPrompt.addButton("Convert to Project")
 
 var didSelectTarget = targetPrompt.show()
@@ -20,12 +21,14 @@ if (!didSelectTarget) {
 		var task = TJSTodo.create()
 		task.title = draft.processTemplate("[[title]]")
 		task.notes = draft.processTemplate("[[body]]")
+		
 	} else if (location == "Today") {
 		var task = TJSTodo.create()
 		task.title = draft.processTemplate("[[title]]")
 		task.notes = draft.processTemplate("[[body]]")
 		task.when = "today"
-	} else if (location == "Project") {
+		
+	} else if (location == "Existing Project") {
 		var taskPrompt = Prompt.create()
 		taskPrompt.title = "Select Project"
 		taskPrompt.addSelect("projectChoice", "Preset Projects", presetProjects.concat(["Other"]), ["Other"], false) 
@@ -84,6 +87,58 @@ if (!didSelectTarget) {
 				task.heading = heading
 			}
 		}
+	} else if (location == "Convert to Project") {
+		var projectPrompt = Prompt.create()
+		projectPrompt.title = "Select Area"
+		projectPrompt.addSelect("areaChoice", "Preset Areas", presetAreas.concat(["Other"]), ["Other"], false) 
+		projectPrompt.addTextField("otherArea", "", "", {placeholder: "Other Area", autocorrect: true, autocapitalization: "words"})
+		projectPrompt.addTextField("tags", "Tags", "", {placeholder: "tag1, tag2, ...", autocorrect: true})
+		projectPrompt.addButton("OK")
+		
+		didSelectArea = projectPrompt.show()
+		if (!didSelectArea) {
+			context.cancel()
+		} else  {
+			var datePrompt = Prompt.create()
+			datePrompt.title = "Dates"
+			
+			var today = new Date()
+			datePrompt.addSwitch("hasDate", "Add Date", false)
+			datePrompt.addDatePicker("date", "Date", today, {mode: "date"})
+			
+			datePrompt.addSwitch("hasDeadline", "Add Deadline", false)
+			datePrompt.addDatePicker("deadline", "Deadline", today, {mode: "date"})
+			
+			datePrompt.addButton("OK")
+			
+			var didSelectDate = datePrompt.show()
+			
+			if (!didSelectDate) {
+				context.cancel()
+			} else {
+				var areaChoice = projectPrompt.fieldValues.areaChoice
+				var otherArea = projectPrompt.fieldValues.otherArea
+				var hasDate = datePrompt.fieldValues.hasDate
+				var date = datePrompt.fieldValues.date
+				var hasDeadline = datePrompt.fieldValues.hasDeadline
+				var deadline = datePrompt.fieldValues.deadline
+				var task = TJSProject.create() 
+				task.title = draft.processTemplate("[[title]]")
+				task.notes = draft.processTemplate("[[body]]")
+				if (hasDate) {
+					task.when = date
+				}
+				if (hasDeadline) {
+					task.deadline = deadline
+				}
+				if (areaChoice == "Other") {
+					task.area = otherArea
+				} else {
+					task.area = areaChoice
+				}
+			}
+		}
+		
 	}
 }
 
